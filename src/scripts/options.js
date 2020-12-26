1cb7e8c1d77b711070b766857ab781925bee43a5
aashu <ashutoshmalviya1000@gmail.com>
2020-12-27 00:32:20 +0530

    add linting and prettier

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -1,20 +1,20 @@
-import getUsageStats from "./utils/getUsageStats.js";
-import { createList } from "./utils/updateDom.js";
+import getUsageStats from './utils/getUsageStats.js'
+import { createList } from './utils/updateDom.js'
 
 async function generateList() {
-  let a = await getUsageStats();
-  createList(a.usageData);
+    let a = await getUsageStats()
+    createList(a.usageData)
 }
-generateList();
+generateList()
 // update page every 1 seconds
-setInterval(generateList, 1000);
+setInterval(generateList, 1000)
 
 // reset button
 
-const resetBtn = document.querySelector("#resetBtn");
-resetBtn.addEventListener("click", () => {
-  chrome.storage.sync.set({ usageData: {} }, function () {
-    alert("data reset successful! updating page in few seconds automatically");
-    generateList();
-  });
-});
+const resetBtn = document.querySelector('#resetBtn')
+resetBtn.addEventListener('click', () => {
+    chrome.storage.sync.set({ usageData: {} }, function () {
+        alert('data reset successful! updating page in few seconds automatically')
+        generateList()
+    })
+})
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,54 +1,48 @@
-import getUsageStats from "./utils/getUsageStats.js";
-import { createList, createListItem } from "./utils/updateDom.js";
-import getTimeString from "./utils/time.js";
+import getUsageStats from './utils/getUsageStats.js'
+import { createList, createListItem } from './utils/updateDom.js'
+import getTimeString from './utils/time.js'
 
 function updateMostVisitedSites(stats) {
-  let timeSpentValues = Object.values(stats);
-  timeSpentValues.sort(function (a, b) {
-    return a - b;
-  });
-  let topTimeSpentValues = timeSpentValues.reverse().slice(0, 5);
-  let topStats = {};
+    let timeSpentValues = Object.values(stats)
+    timeSpentValues.sort(function (a, b) {
+        return a - b
+    })
+    let topTimeSpentValues = timeSpentValues.reverse().slice(0, 5)
+    let topStats = {}
 
-  for (let i in topTimeSpentValues) {
-    for (let key in stats) {
-      if (stats[key] === topTimeSpentValues[i]) {
-        topStats[key] = topTimeSpentValues[i];
-        break;
-      }
+    for (let i in topTimeSpentValues) {
+        for (let key in stats) {
+            if (stats[key] === topTimeSpentValues[i]) {
+                topStats[key] = topTimeSpentValues[i]
+                break
+            }
+        }
     }
-  }
-  createList(topStats);
+    createList(topStats)
 }
 
 function updateCurrentSite(stats) {
-  chrome.tabs.query(
-    {
-      active: true,
-      currentWindow: true,
-    },
-    (tabs) => {
-      let url = tabs[0].url;
-      let hostname = url
-        .replace("http://", "")
-        .replace("https://", "")
-        .split(/[/?#]/)[0];
-      let currentSite = createListItem(
-        hostname,
-        getTimeString(stats[hostname])
-      );
-      let currentSiteSpan = document.querySelector("#currentSite");
-      currentSiteSpan.innerHTML = "";
-      currentSiteSpan.appendChild(currentSite);
-    }
-  );
+    chrome.tabs.query(
+        {
+            active: true,
+            currentWindow: true,
+        },
+        (tabs) => {
+            let url = tabs[0].url
+            let hostname = url.replace('http://', '').replace('https://', '').split(/[/?#]/)[0]
+            let currentSite = createListItem(hostname, getTimeString(stats[hostname]))
+            let currentSiteSpan = document.querySelector('#currentSite')
+            currentSiteSpan.innerHTML = ''
+            currentSiteSpan.appendChild(currentSite)
+        }
+    )
 }
 async function updatePage() {
-  let a = await getUsageStats();
-  const stats = a.usageData;
-  updateMostVisitedSites(stats);
-  updateCurrentSite(stats);
+    let a = await getUsageStats()
+    const stats = a.usageData
+    updateMostVisitedSites(stats)
+    updateCurrentSite(stats)
 }
-updatePage();
+updatePage()
 // update page every 3 seconds
-setInterval(updatePage, 3000);
+setInterval(updatePage, 3000)
